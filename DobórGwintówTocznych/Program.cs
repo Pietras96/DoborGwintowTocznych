75ae5640c536299d6c47774a9b6bd93c8cdc46c9
Piotrek <Piotrek@DESKTOP-1D3S46N>
2022-07-02 11:57:08 +0200

    Add project files.

---
+++ DobórGwintówTocznych/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DobórGwintówTocznych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
